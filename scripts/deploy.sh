ca453622b7d4ebeaabb0d55c565831465ac31eb6
Katrin Torsunova <to.katrin.t@gmail.com>
2026-08-03 16:18:01 +0200

    fix: deploy script (sha)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ cd /home/katrin/projects/echo_
 git fetch origin main
 git reset --hard origin/main
 
-export ECHO_VERSION="$(git rev-parse --short HEAD)"
-echo "Deploying Echo version: ${ECHO_VERSION}"
+echo_version="$(git rev-parse --short HEAD)"
+echo "Deploying Echo version: ${echo_version}"
 
-docker compose up -d --build
-docker compose ps
+ECHO_VERSION="${echo_version}" docker compose up -d --build
+ECHO_VERSION="${echo_version}" docker compose ps
